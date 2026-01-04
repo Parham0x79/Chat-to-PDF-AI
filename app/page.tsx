cdd8137761befa58eca59d50ca1979108d3258bf
Parham <parhamfatemi2000@gmail.com>
2026-01-04 19:16:37 +0330

    landing page completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,107 @@
+import { Button } from "@/components/ui/button";
+import { features } from "@/lib/features";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main
+      className="bg-linear-to-bl flex-1 from-white to-red-500
+        p-3 sm:p-10"
+    >
+      <div
+        className="bg-white xl:max-w-11/12 mx-auto p-6 md:p-24 xl:p-32
+          rounded-md drop-shadow-xl"
+      >
+        <div
+          className="flex flex-col justify-center items-center
+            max-w-7xl mx-auto p-1 lg:p-12"
+        >
+          <div className="max-w-2xl mx-auto">
+            <h2
+              className="text-sm italic lg:text-lg font-mono text-center
+                font-semibold my-5 lg:my-10"
+            >
+              Your Interactive Document companion
+            </h2>
+            <p
+              className="text-2xl font-bold font-sans sm:text-4xl 
+                xl:text-6xl tracking-tight text-center xl:leading-16"
+            >
+              Transform Your PDFs into Interactive Conversations
+            </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <p
+              className="text-sm lg:text-xl font-sans p-1 lg:py-10
+                leading-8 my-10"
+            >
+              Introducing{" "}
+              <span className="font-bold text-red-500">Chat with PDF</span>
+              <br />
+              Upload your Document and the AI will summarize the content and
+              answer your questions. <br />
+              Chat with your PDF to turn static documents into{" "}
+              <span className="font-bold underline">
+                Dynamic conversations
+              </span>{" "}
+              enhancing productivity effortlessly
+            </p>
+          </div>
+          <Button
+            variant={"outline"}
+            className="lg:w-60 lg:h-16 h-12 w-35 bg-red-500 max-lg:text-white text-md font-semibold
+             lg:bg-white xl:hover:bg-red-500 xl:text-xl font-mono"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <Link href={"/dashboard"}>Get Started</Link>
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="relative overflow-hidden">
+          <div className="mx-auto max-w-7xl">
+            <Image
+              src={
+                "https://magicul.io/blog/wp-content/uploads/2024/01/how-to-convert-pdf-to-adobe-illustrator-format.png"
+              }
+              alt="App screenShot"
+              width={1520}
+              height={920}
+              className="mb-[-0%] mt-5 xl:mt-2 rounded-xl shadow-2xl 
+                ring-1 ring-gray-900/10"
+            />
+
+            <div aria-hidden="true" className="relative">
+              <div
+                className="absolute bottom-0 -inset-x-32 bg-linear-to-t
+                from-white/80 pt-[5%]"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+          <dl
+            className="mx-auto max-w-2xl text-base leading-7 text-gray-800
+              grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:mx-0 
+              lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16"
+          >
+            {features.map((item, index) => (
+              <div
+                key={index}
+                className="relative pl-9 border shadow-md rounded-md
+                 p-7 lg:p-10 hover:shadow-xl transition-all"
+              >
+                <dt className="inline font-semibold text-gray-900">
+                  <item.icon
+                    aria-hidden="true"
+                    className="text-red-500 absolute left-1 top-1 w-6 h-6"
+                  />
+                </dt>
+                <dd className="text-lg xl:text-2xl">{item.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </main>
   );
 }
---
+++ lib/features.ts
@@ -0,0 +1,43 @@
+import {
+  BrainCogIcon,
+  EyeIcon,
+  GlobeIcon,
+  MonitorSmartphone,
+  ServerCogIcon,
+  ZapIcon,
+} from "lucide-react";
+
+export const features = [
+  {
+    name: "Store your PDF Documents",
+    description: "Keep all your important files securely",
+    icon: GlobeIcon,
+  },
+  {
+    name: "Blazing fast Responses",
+    description: "Lightning fast answers to your queries",
+    icon: ZapIcon,
+  },
+  {
+    name: "Chat Memorization",
+    description: "out AI chatbot remembers previous conversations",
+    icon: BrainCogIcon,
+  },
+
+  {
+    name: "Interactive PDF viewer",
+    description: "Engage with your PDF with AI",
+    icon: EyeIcon,
+  },
+
+  {
+    name: "Cloud Backup",
+    description: "Your docoments backed up on the cloud",
+    icon: ServerCogIcon,
+  },
+  {
+    name: "Responsive Across Devices",
+    description: "Access and chat with PDFs on any device",
+    icon: MonitorSmartphone,
+  },
+];
